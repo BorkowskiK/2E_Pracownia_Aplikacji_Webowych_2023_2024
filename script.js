c3852d770028b2a7b126b0bb72b80288cd0fe2ef
BorkowskiK <113013923+BorkowskiK@users.noreply.github.com>
2024-06-03 09:29:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function change1() {
+    var color = document.getElementById("prawy");
+    color.style.backgroundColor = "Indigo";
+}
+
+function change2() {
+    var color = document.getElementById("prawy");
+    color.style.backgroundColor = "Steelblue";
+}
+
+function change3() {
+    var color = document.getElementById("prawy");
+    color.style.backgroundColor = "olive";
+}
